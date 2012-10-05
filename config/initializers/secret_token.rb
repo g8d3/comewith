6d83fcd27482a59ba48c4962313f5c9d9b261af4
Juan David Pastas <juandavid1707@gmail.com>
2012-10-04 20:03:23 -0500

    initial commit, facebook sign in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comewith::Application.config.secret_token = 'eabede5c8bba2d52ae60c6afae4f4a2585555b1f52426495b637d206c2f354593ac64567cde14c2dbfb94ec34ef5f710dce458deb63c127380e1b3c7844bd3e8'
